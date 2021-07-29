e395325240392ca811dada6f6001f262e5bd433d
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-07-29 10:25:50 +0200

    set up localStorage service for getting, setting and removing localStorage

--- src/app/services/local-storage.service.ts
+++ src/app/services/local-storage.service.ts
@@ -6,4 +6,25 @@ import { Injectable } from '@angular/core';
 export class LocalStorageService {
 
   constructor() { }
+
+  setStorage(key: string, data: any): void {
+    try {
+      localStorage.setItem(key, JSON.stringify(data));
+    } catch (err) {
+      console.error('Error saving data to localStorage', err);
+    }
+  }
+
+  getStorage(key: string) {
+    try {
+      return JSON.parse(localStorage.getItem(key) || '[]');
+    } catch (err) {
+      console.error('Error getting data from localStorage', err);
+      return null;
+    }
+  }
+
+  removeFromStorage(key: string) {
+    localStorage.removeItem(key);
+  }
 }
