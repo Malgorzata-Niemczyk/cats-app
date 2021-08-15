b22f0b826af785fab0325c18c584a157993c01dc
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-08-16 01:24:15 +0200

    add placeholder attributes to the input elements in the form and refactor the dialog services

--- src/app/services/confirm-dialog.service.ts
+++ src/app/services/confirm-dialog.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ConfirmDialogData } from '../models/confirm-modal-data';
 import { ConfirmDialogComponent } from '../components/confirm-dialog/confirm-dialog.component';
-import { MatDialog, MatDialogConfig, MatDialogRef} from '@angular/material/dialog';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 
 
 @Injectable({
@@ -11,15 +11,13 @@ export class ConfirmDialogService {
 
   constructor(private dialog: MatDialog) { }
 
-  dialogRef: MatDialogRef<ConfirmDialogComponent>;
-
   public openDialog(data: ConfirmDialogData): void {
     const dialogConfig = new MatDialogConfig();
     dialogConfig.disableClose = true;
     dialogConfig.width = '390px';
     dialogConfig.data = data;
 
-    this.dialogRef = this.dialog.open(ConfirmDialogComponent, dialogConfig)
+    this.dialog.open(ConfirmDialogComponent, dialogConfig)
   }
 
 }
--- src/app/services/save-dialog.service.ts
+++ src/app/services/save-dialog.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { MatDialog, MatDialogConfig, MatDialogRef} from '@angular/material/dialog';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { AddCatDialogComponent } from '../components/add-cat-dialog/add-cat-dialog.component';
 import { NewCat } from '../models/save-modal-data';
 
@@ -11,15 +11,13 @@ export class SaveDialogService {
 
   constructor(private dialog: MatDialog) { }
 
-  dialogRef: MatDialogRef<AddCatDialogComponent>;
-
   public openDialog(data: NewCat): void {
     const dialogConfig = new MatDialogConfig();
     dialogConfig.disableClose = true;
-    dialogConfig.width = '480px';
+    dialogConfig.width = '460px';
     dialogConfig.data = data;
 
-    this.dialogRef = this.dialog.open(AddCatDialogComponent, dialogConfig)
+    this.dialog.open(AddCatDialogComponent, dialogConfig);
   }
 
 }
